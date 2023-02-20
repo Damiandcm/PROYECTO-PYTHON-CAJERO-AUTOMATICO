d17fdc3b1be1e4b17fe279af6e9a02fa78535449
Damian Campos <isaaccamposmon@gmail.com>
2023-02-20 07:51:56 -0600

    Ultimos Arreglos al servidor

--- servidor/servidor_bancario.py
+++ servidor/servidor_bancario.py
@@ -9,33 +9,161 @@ def registrar(informacion, socket):
 
     nombre = informacion[1]
     contra = informacion[2]
-    # primer_ingreso = informacion[3] #  falta agregar
+    primer_ingreso = informacion[3]
+
+    # Abir archivo
 
     try:
-        with open('Datos_Bancarios.json') as file:
+        with open("Datos_Bancarios.json") as file:
             archivo = json.load(file)
-            archivo['clientesbanco'].append({
-                'nombre': nombre,
-                'contra': contra
-            })
 
-        with open('Datos_Bancarios.json', 'w') as file: 
-            json.dump(archivo, file, indent=4)
+            if nombre in archivo.keys():
+                socket.send_string("Rechasado")
 
-        
+                print("Peticion rechasada. El usuario \"{}\" ya esta registrado".format(nombre))
+
+            else:
+                archivo[nombre] = [nombre, contra, primer_ingreso]
+
+                with open("Datos_Bancarios.json", "w") as file: 
+                    json.dump(archivo, file, indent=4)
+
+                socket.send_string("Anadido a la base de datos")
+
+                print("El usuario {} ha sido anadido a la base de datos".format(nombre))
 
     except FileNotFoundError:
         archivo = {}
-        archivo['clientesbanco'] = []
-        archivo['clientesbanco'].append({
-            'nombre': nombre,
-            'contra': contra
-        })
-        with open('Datos_Bancarios.json', 'w') as file: 
+        archivo[nombre] = [nombre, contra, primer_ingreso]
+        with open("Datos_Bancarios.json", "w") as file: 
             json.dump(archivo, file, indent=4)
 
-    socket.send_string("Anadido a la base de datos")
-    print('El usuario {} ha sido anadido a la base de datos'.format(nombre))
+            socket.send_string("Anadido a la base de datos")
+    
+            print("El usuario {} ha sido anadido a la base de datos".format(nombre))
+
+def inicio_sesion(informacion, socket):
+
+    # Contenido del mensaje
+
+    nombre = informacion[1]
+    contra = informacion[2]
+
+    # Abir archivo
+
+    try:
+        with open("Datos_Bancarios.json") as file:
+            archivo = json.load(file)
+
+            if nombre in archivo.keys():
+                #nombre_reg = archivo[nombre][0]
+                contra_reg = archivo[nombre][1]
+                #dinero_reg = archivo[nombre][2]
+
+                if contra != contra_reg:
+                    socket.send_string("contrasena incorrecta")
+                    print("Usuario \"{}\" encontrado. Contrasena incorrecta".format(nombre))
+                else:
+                    socket.send_string("contrasena correcta")
+                    print("Usuario \"{}\" encontrado. Contrasena correcta".format(nombre))
+            else:
+                socket.send_string("nombre no registrado")
+                print("Usuario \"{}\" no encontrado".format(nombre))
+    except FileNotFoundError:
+        socket.send_string("nombre no registrado")
+        print("Usuario \"{}\" no encontrado".format(nombre))
+
+def agregar_monto(informacion, socket):
+
+    # Contenido del mensaje
+
+    nombre = informacion[1]
+
+    try:
+        monto_agregar = int(informacion[2])
+
+        # Abrir archivo Json 
+
+        with open("Datos_Bancarios.json") as file:
+            archivo = json.load(file)
+
+        registro_usuario = archivo[nombre]
+
+        dinero_anterior = int(registro_usuario[2])
+        
+        registro_usuario[2] = str(dinero_anterior + monto_agregar)
+
+        with open("Datos_Bancarios.json", "w") as file: 
+            json.dump(archivo, file, indent=4)
+        
+        socket.send_string("Monto anadido")
+        
+        print("Monto anadido")
+
+    except ValueError:
+        socket.send_string("ValueError")
+
+
+def consultar(informacion, socket):
+
+    # Contenido del mensaje
+
+    nombre = informacion[1]
+    
+    # Abrir archivo Json 
+
+    with open("Datos_Bancarios.json") as file:
+        archivo = json.load(file)
+
+    registro_usuario = archivo[nombre]
+
+    fondos = registro_usuario[2]
+
+    # Enviar informacion. Fondos
+
+    socket.send_string(fondos)
+    print('Enviado: Fondos registrados')
+
+def sacar(informacion, socket):
+
+    # Contenido del mensaje
+
+    nombre = informacion[1]
+
+    try:
+        monto_sacar = int(informacion[2])
+
+        # Abrir archivo Json 
+
+        with open("Datos_Bancarios.json") as file:
+            archivo = json.load(file)
+
+        registro_usuario = archivo[nombre]
+
+        dinero_anterior = int(registro_usuario[2])
+
+        if dinero_anterior >= monto_sacar:
+        
+            registro_usuario[2] = str(dinero_anterior - monto_sacar)
+
+            with open("Datos_Bancarios.json", "w") as file: 
+                json.dump(archivo, file, indent=4)
+            
+            socket.send_string("Monto retirado")
+            
+            print("Monto retirado")
+
+        else:
+
+            socket.send_string("mas de lo registrado")
+            
+            print("Error: Desea sacar mas dinero de lo que tiene registrado")
+
+
+    except ValueError:
+        socket.send_string("ValueError")
+
+
 def mensajeria():
 
     # Crear contexto. Conectar socket
@@ -46,15 +174,24 @@ def mensajeria():
     # Resivir mensajes
 
     while True:
+        
         mensaje = socket.recv()
         
         mensaje = mensaje.decode("utf-8")
-        informacion = mensaje.split('/')
+        informacion = mensaje.split("/")
         opcion = informacion[0]
         
-        if opcion == 'Registrar':
+        if opcion == "Registrar":
             registrar(informacion, socket)
+        elif opcion == "inicio sesion":
+            inicio_sesion(informacion, socket)
+        elif opcion == "Agregar":
+            agregar_monto(informacion, socket)
+        elif opcion == 'Consultar':
+            consultar(informacion, socket)
+        elif opcion == "Sacar":
+            sacar(informacion, socket)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mensajeria()
